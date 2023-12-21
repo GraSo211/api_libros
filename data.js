ce1c063b9461cf45f59f088f5c6c768b268e95f3
SANTIAGO <90029@sistemas.frc.utn.edu.ar>
2023-12-20 22:01:40 -0300

    ejercicio 4

---
+++ data.js
@@ -0,0 +1,15 @@
+const libros = [
+    { id: '1', titulo: 'Libro 1', autor: 'Autor 1' },
+    { id: '2', titulo: 'Libro 2', autor: 'Autor 2' },
+    { id: '3', titulo: 'Libro 3', autor: 'Autor 3' },
+    { id: '4', titulo: 'Libro 1', autor: 'Autor 1' },
+    { id: '5', titulo: 'Libro 2', autor: 'Autor 2' },
+    { id: '6', titulo: 'Libro 3', autor: 'Autor 3' },
+    { id: '7', titulo: 'Libro 1', autor: 'Autor 1' },
+    { id: '8', titulo: 'Libro 2', autor: 'Autor 2' },
+    { id: '9', titulo: 'Libro 3', autor: 'Autor 3' },
+    { id: '10', titulo: 'Libro 1', autor: 'Autor 1' },
+    { id: '12', titulo: 'Libro 2', autor: 'Autor 2' },
+    { id: '13', titulo: 'Libro 3', autor: 'Autor 3' }
+    ];
+    module.exports = libros;
